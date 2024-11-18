a4077b2756913082577c430477a41aba1f7d3162
vsbron <vsbron@gmail.com>
2024-11-18 10:26:00 +0300

    Configure HEAD element. Add favicons & postcss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
   // Returned JSX
   return (
-    <main className="bg-black">
-      <h1>BroNflix project</h1>
-      <h4>Under construction</h4>
+    <main className="bg-stone-950 text-stone-50">
+      <img src="/logo-letter.png" alt="BroNflix Logo" />
+      <h1 className="text-8xl -mt-40">BroNflix project</h1>
+      <h4 className="text-4xl mb-40">Under construction</h4>
     </main>
   );
 }
