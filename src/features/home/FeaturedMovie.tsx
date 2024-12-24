cc861e285b49c1a4d8bab1034187a05c7f85e122
vsbron <vsbron@gmail.com>
2024-12-24 10:52:05 +0300

    Completed UI for Featured Movie section

--- src/features/home/FeaturedMovie.tsx
+++ src/features/home/FeaturedMovie.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Heading from "../../components/Heading";
 import { useFeatureMovie } from "./useFeatureMovie";
 
@@ -9,21 +10,38 @@ function FeaturedMovie() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error fetching movie</div>;
 
+  const overview = movie?.overview || "";
+  const truncatedOverview =
+    overview.length > 165
+      ? `${overview.slice(0, 165).trim().split(" ").slice(0, -1).join(" ")}...`
+      : overview;
+
   // Returned JSX
   return (
     <section>
       <Heading>WHAT'S HOT</Heading>
       <div
-        className="relative w-full h-[50rem] flex items-end justify-start text-white max-w-[65vw] p-20"
         style={{
           backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
-          backgroundSize: "cover",
           backgroundPosition: "top center",
         }}
+        className="relative w-full h-[50rem] flex items-end justify-start text-white max-w-[65vw] bg-cover"
       >
-        <div className="max-w-[50%]">
-          <h1 className="text-6xl font-bold mb-4">{movie?.title}</h1>
-          <p className="text-xl">{movie?.overview}</p>
+        <div className="p-10 pt-96 pr-[50%] bg-gradient-to-t from-stone-950 to-transparent">
+          <h1 className="text-7xl font-bold mb-4">{movie?.title}</h1>
+          <p className="text-2xl">{truncatedOverview}</p>
+          <div className="mt-6 font-bold flex gap-10">
+            <div className="bg-red-950 rounded-lg inline-block hover:bg-stone-50 hover:text-red-950 ">
+              <Link className="inline-block py-3 px-6" to="/movies/something">
+                READ MORE
+              </Link>
+            </div>
+            <div className="bg-red-950 rounded-lg inline-block hover:bg-stone-50 hover:text-red-950 ">
+              <Link className="inline-block py-3 px-6" to="/movies/something">
+                WATCH TRAILER
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </section>
