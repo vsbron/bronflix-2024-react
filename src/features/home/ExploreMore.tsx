b78ca216388adc8f5e3572b10956498e21526616
vsbron <vsbron@gmail.com>
2024-12-29 01:50:50 +0300

    Edit the More Content to explore section

--- src/features/home/ExploreMore.tsx
+++ src/features/home/ExploreMore.tsx
@@ -7,21 +7,28 @@ function ExploreMore() {
   return (
     <div>
       <Heading as="h2">More content to explore</Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eveniet
-        libero ab similique nemo molestias repellat id cupiditate animi ut?
-        Incidunt adipisci enim eos soluta minus officiis nemo dignissimos
-        reiciendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis in
-        aperiam repellendus ad doloremque suscipit incidunt quia. Voluptate
-        molestias perferendis sint, facere tempora molestiae iure ex magni eum
-        aliquam! Distinctio. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Illo eveniet libero ab similique nemo molestias repellat id
-        cupiditate animi ut? Incidunt adipisci enim eos soluta minus officiis
-        nemo dignissimos reiciendis.
-      </p>
+      <div className="w-1/2">
+        <p>
+          Dive into our expansive library of movies, TV series, and celebrity
+          profiles. Explore action-packed blockbusters, heartwarming dramas, and
+          mind-bending thrillers, with detailed information on each title,
+          including plot summaries, cast, and ratings. Discover trending titles,
+          top-rated classics, or search for something specific using the{" "}
+          <strong>search bar</strong> at the top of the page. Browse our{" "}
+          <strong>genres</strong> and curated <strong>movie collections</strong>{" "}
+          from franchises to themed lists - and dive into each{" "}
+          <strong>season</strong> and <strong>episode</strong> of your favorite
+          shows.
+        </p>
+        <p>
+          If you're a member, you can save movies and shows to your{" "}
+          <strong>favorites</strong> list, or add items to your{" "}
+          <strong>future watch list</strong> to keep track of what you're
+          excited to see. Stay up-to-date with everything trending in the world
+          of entertainment, whether you're discovering new content or revisiting
+          timeless classics.
+        </p>
+      </div>
       <div className="flex gap-8 mt-8">
         <Button>
           <Link className="inline-block py-3 px-6" to="/movies">
