3e3acb654feb2ee6ebc1e5851ac34a6ad6ac2ab5
vsbron <vsbron@gmail.com>
2025-01-09 17:18:27 +0300

    Minor CSS fixes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import User from "./User";
 function Header() {
   // Returned JSX
   return (
-    <header className="absolute top-0 left-0 right-0 px-12 py-4 flex justify-end items-center gap-10">
+    <header className="absolute top-0 left-1/2 right-0 px-12 py-4 flex justify-end items-center gap-10">
       <Search />
       <User />
     </header>
