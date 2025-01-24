994a15cc29a10d541785fddcb7d16e78d2c070f5
vsbron <vsbron@gmail.com>
2025-01-24 15:00:19 +0300

    Posters of seasons roughly featured

--- src/features/showDetails/ShowSeasons.tsx
+++ src/features/showDetails/ShowSeasons.tsx
@@ -1,5 +1,6 @@
 import Heading from "@/components/ui/Heading";
-import { PREVIEWS_GAP_CLASS } from "@/lib/constants";
+import { BlackGradientToTop } from "@/components/ui/Overlays";
+import { MEDIA_IMG_URL, PREVIEWS_GAP_CLASS } from "@/lib/constants";
 import { ISeason } from "@/lib/typesAPI";
 
 function ShowSeasons({ seasons }: { seasons: ISeason[] }) {
@@ -16,7 +17,18 @@ function ShowSeasons({ seasons }: { seasons: ISeason[] }) {
       <Heading as="h3">Seasons</Heading>
       <div className={`flex ${PREVIEWS_GAP_CLASS}`}>
         {onlySeasons.map((season) => (
-          <div key={season.id}>{season.name}</div>
+          <div
+            key={season.id}
+            className="w-[20rem] h-[30rem] bg-cover rounded-xl relative"
+            style={{
+              backgroundImage: `url(${MEDIA_IMG_URL}w500${season.poster_path})`,
+            }}
+          >
+            <BlackGradientToTop height="10rem" />
+            <div className="absolute bottom-4 left-4 z-10 font-heading">
+              {season.name}
+            </div>
+          </div>
         ))}
       </div>
     </section>
