d1420d191285fe5ca523c1ba3ed31a02468ec307
vsbron <vsbron@gmail.com>
2025-01-26 16:31:13 +0300

    Add propet components for Season page

--- src/pages/Season.tsx
+++ src/pages/Season.tsx
@@ -1,13 +1,11 @@
 import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
+import { META_TITLE_END } from "@/lib/metaTags";
 import { ISeason } from "@/lib/typesAPI";
 import { getSeason } from "@/services/apiShows";
-import Heading from "@/components/ui/Heading";
-import MediaHero from "@/components/MediaHero";
-import ScorePreview from "@/components/ScorePreview";
-import { formatDate } from "@/utils/helpers";
-import IconWrapper from "@/components/IconWrapper";
-import { CalendarIcon } from "@heroicons/react/24/outline";
+
+import SeasonDetails from "@/features/showDetails/SeasonDetails";
 
 // Season data loader
 export const seasonLoader = async ({
@@ -23,39 +21,28 @@ function Season() {
   // Getting the season data from the loader
   const season = useLoaderData() as ISeason;
 
-  const {
-    air_date,
-    name,
-    overview,
-    poster_path,
-    season_number,
-    vote_average: score,
-  } = season;
-
   console.log(season);
 
   // Returned JSX
   return (
-    <section>
-      <Heading>{name}</Heading>
-      <MediaHero media={season} posterWidth="40rem">
-        <div className="relative z-10 flex flex-col gap-3">
-          <div className="text-gray-400">
-            <ScorePreview score={score} isHighlighted={true} isBig={true} />
-          </div>
-          <div className="text-[4rem] -my-5 font-heading">{name}</div>
-          <div className="contents text-2xl">
-            <div className="flex gap-8">
-              <IconWrapper icon={<CalendarIcon />}>
-                {formatDate(air_date)}
-              </IconWrapper>
-            </div>
-            <div className="flex gap-8 mb-2"></div>
-          </div>
-          {overview && <div className="max-w-[65rem] mb-6">{overview}</div>}
-        </div>
-      </MediaHero>
-    </section>
+    <>
+      {/* Meta data */}
+      <Helmet>
+        <title>{`${season.name} - Season${META_TITLE_END}`}</title>
+        <meta
+          name="description"
+          content={`Discover the overview of ${season.name}, explore the cast and crew, watch the trailer, and dive into all the details about the show's release.`}
+        />
+        <meta
+          name="keywords"
+          content={`${season.name}, show, TV series, plot, cast, crew, release date, creator, director, actors, genres, rating, episodes, seasons, trailer`}
+        />
+        <meta name="robots" content="index,follow" />
+      </Helmet>
+
+      {/* Content */}
+      <SeasonDetails season={season} />
+    </>
   );
 }
 
