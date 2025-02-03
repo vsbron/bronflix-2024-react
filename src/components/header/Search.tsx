d86d88747a7e5b093738030aac0c97bb59606be3
vsbron <vsbron@gmail.com>
2025-02-03 09:41:45 +0300

    Add debouncing to the search query fetching

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { debounce } from "lodash";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 import { MEDIA_URL } from "@/lib/constants";
@@ -15,56 +16,59 @@ function Search() {
   // Getting the navigate function from useNavigate hook
   const navigate = useNavigate();
 
-  // Use Effect that fetches the brief search results as user enters the search query
-  useEffect(() => {
-    // Creating controller for cleanup function
-    const controller = new AbortController();
-    const { signal } = controller;
+  // Fetch data based on a search query function
+  const fetchSearch = async () => {
+    // Trimming query
+    if (!inputText.trim()) return;
 
-    // Async function for data fetch
-    const fetchSearch = async () => {
-      try {
-        // Fetching the data
-        const res = await fetch(
-          `${MEDIA_URL}/search/multi?api_key=${
-            import.meta.env.VITE_TMDB_API_KEY
-          }&query=${inputText}`,
-          { signal }
-        );
+    try {
+      // Fetching the data
+      const res = await fetch(
+        `${MEDIA_URL}/search/multi?api_key=${
+          import.meta.env.VITE_TMDB_API_KEY
+        }&query=${inputText}`
+      );
 
-        // Guard clause
-        if (!res.ok) {
-          throw new Error(`Error: ${res.statusText}`);
-        }
-
-        // Handling the fetched data
-        const data = await res.json();
-        const briefData = data.results.slice(0, 3);
-        const totalResults = data.total_results;
-
-        // Set the state with first three results and the total results number
-        setBriefResults({ briefData, totalResults });
-      } catch (e: unknown) {
-        // Checking if error message is because of cleanup function
-        if (e instanceof DOMException && e.name === "AbortError") {
-          console.log("Fetch aborted"); // Log for controlled aborts
-        } else {
-          // Else, handle error
-          const errorMessage = e instanceof Error ? e.message : String(e);
-          console.error(errorMessage);
-          setError(errorMessage);
-        }
+      // Guard clause
+      if (!res.ok) {
+        throw new Error(`Error: ${res.statusText}`);
       }
-    };
 
-    // Calling the async function
-    fetchSearch();
+      // Handling the fetched data
+      const data = await res.json();
+      const briefData = data.results.slice(0, 3);
+      const totalResults = data.total_results;
 
-    // Cleanup function
+      // Set the state with first three results and the total results number
+      setBriefResults({ briefData, totalResults });
+    } catch (e: unknown) {
+      // Checking if error message is because of cleanup function
+      if (e instanceof DOMException && e.name === "AbortError") {
+        console.log("Fetch aborted"); // Log for controlled aborts
+      } else {
+        // Else, handle error
+        const errorMessage = e instanceof Error ? e.message : String(e);
+        console.error(errorMessage);
+        setError(errorMessage);
+      }
+    }
+  };
+
+  // Memoized debounced fetchSearch function
+  const debouncedFetchSearch = useCallback(debounce(fetchSearch, 300), [
+    inputText,
+  ]);
+
+  // Use Effect for calling fetch function when query is updated
+  useEffect(() => {
+    // Calling the async function if the query length is longer than three characters
+    inputText.trim().length > 3 && debouncedFetchSearch();
+
+    // Cleanup function on component unmount
     return () => {
-      controller.abort(); // Abort fetch on unmount
+      debouncedFetchSearch.cancel();
     };
-  }, [inputText]);
+  }, [inputText, debouncedFetchSearch]);
 
   // Search submission handler
   const handleSearch = (e: React.FormEvent) => {
