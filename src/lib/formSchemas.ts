0f199b8dc9fd07a694bc4fd3fb8eb8d91823d3b5
vsbron <vsbron@gmail.com>
2025-02-10 16:59:21 +0300

    Tie password with password confirm in zod

--- src/lib/formSchemas.ts
+++ src/lib/formSchemas.ts
@@ -1,5 +1,23 @@
 import { z } from "zod";
 
+// Schema for sign in form
+export const signInFormSchema = z.object({
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z.string().min(1, "Password is required"),
+});
+
+// Schema for sign up form
+export const signUpFormSchema = z.object({
+  name: z.string().min(2, "Name must be at least 2 characters"),
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z.string().min(1, "Password is required"),
+  confirmPassword: z.string().min(1, "Password is required"),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"],
+});;
+
+// Schema for contact form
 export const contactFormSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters"),
   age: z.string().min(1, { message: "Select your age range" }),
@@ -8,16 +26,4 @@ export const contactFormSchema = z.object({
     .string()
     .min(1, "Message is required")
     .max(500, "Message is too long"),
-});
-
-export const signInFormSchema = z.object({
-  email: z.string().email("Invalid email address").min(1, "Email is required"),
-  password: z.string().min(1, "Password is required"),
-});
-
-export const signUpFormSchema = z.object({
-  name: z.string().min(2, "Name must be at least 2 characters"),
-  email: z.string().email("Invalid email address").min(1, "Email is required"),
-  password: z.string().min(1, "Password is required"),
-  confirmPassword: z.string().min(1, "Password is required"),
 });
