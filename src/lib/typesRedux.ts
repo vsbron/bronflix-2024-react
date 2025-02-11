d363d8b2b7347133ce1844ea22412c57c0f982c2
vsbron <vsbron@gmail.com>
2025-02-11 17:28:00 +0300

    Finish the auth slice

--- src/lib/typesRedux.ts
+++ src/lib/typesRedux.ts
@@ -2,3 +2,12 @@ import store from "@/redux/store";
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+// Auth slice
+export interface IAuthState {
+  user: {
+    uid: string | null;
+    email: string | null;
+  };
+  isAuthenticated: boolean;
+}
--- src/redux/reducers/authReducer.ts
+++ src/redux/reducers/authReducer.ts
@@ -1,9 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { IAuthState } from "@/lib/typesRedux";
+
 // Creating initial state
-const initialState = {};
+const initialState: IAuthState = {
+  user: {
+    uid: null,
+    email: null,
+  },
+  isAuthenticated: false,
+};
 
 // Creating the slice
-const authSlice = createSlice({ name: "auth", initialState, reducers: {} });
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    // Logging in
+    signIn(state, action) {
+      state.user = { uid: action.payload.uid, email: action.payload.email };
+      state.isAuthenticated = true;
+    },
+    // Creating new account
+    signUp(state, action) {
+      state.user = { uid: action.payload.uid, email: action.payload.email };
+      state.isAuthenticated = true;
+    },
+    // Logging out
+    signOut(state) {
+      state.user = { uid: null, email: null };
+      state.isAuthenticated = false;
+    },
+  },
+});
 
+// Exporting everything out
+export const { signIn, signUp, signOut } = authSlice.actions;
 export default authSlice.reducer;
