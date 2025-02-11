15b1b14c24aee88573d99cda215fb50fac1c6217
vsbron <vsbron@gmail.com>
2025-02-11 17:31:32 +0300

    Whole proejct Redux wrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
 
-import "./index.css";
+import "@/index.css";
+import store from "@/redux/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
