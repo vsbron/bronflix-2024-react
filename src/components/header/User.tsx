788aa5a76a1454c2491db7948b2a6b10bc89c064
vsbron <vsbron@gmail.com>
2025-02-14 13:59:37 +0300

    Add Custom avatar to the fresh user

--- src/components/header/User.tsx
+++ src/components/header/User.tsx
@@ -2,13 +2,17 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signOut } from "firebase/auth";
 
+import { clearUserData, useUser } from "@/redux/reducers/userReducer";
 import { auth } from "@/utils/firebase";
 
 import Button from "@/components/ui/Button";
-import avatar from "@/assets/profile.png";
-import { clearUserData } from "@/redux/reducers/userReducer";
+import noAvatarPNG from "@/assets/noAvatar.png";
+import noAvatarWEBP from "@/assets/noAvatar.webp";
 
 function User() {
+  // Getting the user name from the redux
+  const { name } = useUser();
+
   // Getting the dispatch and navigate functions
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -23,14 +27,17 @@ function User() {
   // Returned JSX
   return (
     <div className="flex gap-6 items-center">
-      <img
-        src={avatar}
-        className="rounded-full"
-        width={50}
-        height={50}
-        alt="BroN"
-        title="BroN avatar"
-      />
+      <picture>
+        <source srcSet={noAvatarWEBP} type="image/webp" />
+        <img
+          src={noAvatarPNG}
+          className="rounded-full"
+          width={50}
+          height={50}
+          alt={name}
+          title={`${name} avatar`}
+        />
+      </picture>
       <Button>
         <Link to="/profile">PROFILE</Link>
       </Button>
