bdf5d2ecc475070d9e201c07e02bfde69a34a25a
vsbron <vsbron@gmail.com>
2025-03-16 00:43:25 +0300

    Add new service function for fetching the genres

---
+++ src/services/apiGenres.ts
@@ -0,0 +1,33 @@
+import { MEDIA_URL } from "@/lib/constants";
+import { APIFetchType, IGenre } from "@/lib/typesAPI";
+
+// API for getting the genres list
+export async function getGenres(type: APIFetchType): Promise<IGenre[]> {
+  try {
+    // Fetch the data
+    const response = await fetch(
+      `${MEDIA_URL}genre/${type}/list?api_key=${
+        import.meta.env.VITE_TMDB_API_KEY
+      }`
+    );
+
+    // Guard clause
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch the ${type} genres data: ${response.statusText}`
+      );
+    }
+
+    // Getting the actual data
+    const data = await response.json();
+
+    // Ensure data.results not empty and is an array
+    if (!data.genres || !Array.isArray(data.genres)) return [];
+
+    // Return the genres list
+    return data.genres;
+  } catch (error: unknown) {
+    console.error(error);
+    throw new Error(`An error occurred while fetching ${type} genres data`);
+  }
+}
