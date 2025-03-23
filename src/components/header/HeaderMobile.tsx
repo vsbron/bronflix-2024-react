1f92845f4146a2ecd41dc09663d7c3020377f055
vsbron <vsbron@gmail.com>
2025-03-24 02:07:33 +0300

    Add new logo. Basic UI styles for Mobile Header

--- src/components/header/HeaderMobile.tsx
+++ src/components/header/HeaderMobile.tsx
@@ -1,16 +1,7 @@
-import { useUser } from "@/redux/reducers/userReducer";
-
-import Authentication from "@/components/header/Authentication";
-import Search from "@/components/header/Search";
-import User from "@/components/header/User";
-
 function HeaderMobile() {
-  // Getting the user id
-  const { uid } = useUser();
-
   // Returned JSX
   return (
-    <header className="absolute top-0 left-1/4 lg:left-1/2 right-0 px-12 my-6 h-[4.725rem] flex justify-end items-center gap-10"></header>
+    <header className="fixed inset-0 bottom-auto px-6 py-12 z-20 flex justify-between bg-black"></header>
   );
 }
 
