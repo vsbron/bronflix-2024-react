aaf927a212a4efb34dffda7be581b2d18b353a0d
vsbron <vsbron@gmail.com>
2025-03-24 01:22:45 +0300

    Install React Responsive package

--- src/components/header/Sidebar.tsx
+++ src/components/header/Sidebar.tsx
@@ -10,9 +10,9 @@ function Sidebar() {
   // Returned JSX
   return (
     <aside
-      className={`fixed left-0 right-0 md:right-auto top-0 bottom-auto md-bottom-0 py-5 px-8 flex flex-col z-30 transition-all duration-200 w-full md:w-[7.5rem] ${
+      className={`fixed left-0 top-0 bottom-0 py-5 px-8 flex flex-col z-30 transition-all duration-200 w-[7.5rem] ${
         isHovered ? "sidebar--expanded" : ""
-      }  opacity-0 pointer-events-none`}
+      }`}
       style={{
         backgroundImage:
           "linear-gradient(to right, var(--color-black) 40%, transparent)",
