1278fddd192c2d99a5505ccca77a9d07d15cb3fa
vsbron <vsbron@gmail.com>
2025-03-25 01:50:40 +0300

    Add more styles to Mobile menu

--- src/components/header/MobileNavCont.tsx
+++ src/components/header/MobileNavCont.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import Hamburger from "@/components/header/Hamburger";
+import Button from "@/components/ui/Button";
+import Heading from "@/components/ui/Heading";
 
 function MobileNavCont() {
   // Setting the state for the menu
@@ -10,22 +13,58 @@ function MobileNavCont() {
   return (
     <div>
       <Hamburger isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-      <div
-        className={`fixed inset-0 bg-black -z-10 flex flex-col justify-center items-center
+      <nav
+        className={`fixed inset-0 bg-black -z-10 flex flex-col justify-start items-center transition-all p-6 pt-24
           ${
             isMenuOpen
               ? "opacity-1 pointer-events-auto"
               : "opacity-0 pointer-events-none"
           }`}
       >
-        <ul>
-          <li>Home</li>
-          <li>Movies</li>
-          <li>Shows</li>
-          <li>Profile</li>
-          <li>Sign Out</li>
-        </ul>
-      </div>
+        <div
+          className={`transition-all w-full mb-6 ${
+            isMenuOpen
+              ? "translate-y-0 delay-100 opacity-1"
+              : "translate-y-60 delay-0 opacity-0"
+          }`}
+        >
+          <Heading as="h2">Navigation</Heading>
+          <ul className="m-0 flex flex-col gap-4 text-3xl">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li>
+              <Link to="/shows">Shows</Link>
+            </li>
+            <li>
+              <Link to="/about-us">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact-us">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        <div
+          className={`transition-all w-full ${
+            isMenuOpen
+              ? "translate-y-0 delay-200 opacity-1"
+              : "translate-y-60 delay-0 opacity-0"
+          }`}
+        >
+          <Heading as="h2">User</Heading>
+          <div className="flex flex-col gap-4 items-start">
+            <Button>
+              <a href="/profile">Profile</a>
+            </Button>
+            <Button>
+              <span>Sign Out</span>
+            </Button>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
