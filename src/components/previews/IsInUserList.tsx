a7467645a62c102442373d4ea1c38720cade7a22
vsbron <vsbron@gmail.com>
2025-03-27 02:23:01 +0300

    Adjust User UI icons for mobile

--- src/components/previews/IsInUserList.tsx
+++ src/components/previews/IsInUserList.tsx
@@ -10,7 +10,7 @@ function IsInUserList({ type, id }: MediaTypeAndId) {
   // Returned JSX
   return (
     <>
-      <div className="absolute top-3 right-3 z-10 w-[2.2rem] flex flex-col gap-2">
+      <div className="absolute top-3 right-3 z-10 w-[1.8rem] md:w-[2.2rem] flex flex-col gap-2">
         {isLiked && (
           <div className="bg-red-800 rounded-full p-1">
             <HeartIcon className="text-red-200" />
@@ -23,7 +23,7 @@ function IsInUserList({ type, id }: MediaTypeAndId) {
         )}
       </div>
       {isRated && (
-        <div className="bg-purple-800 rounded-xl px-1 pr-2 py-0 absolute left-3 top-12 flex items-center gap-0.5 text-xl text-purple-200">
+        <div className="bg-purple-800 rounded-xl px-1 pr-2 py-0 absolute left-2 md:left-3 top-10 md:top-12 flex items-center gap-0.5 text-[1rem] md:text-xl text-purple-200">
           <StarIcon className="w-5" />
           {isRated.rate}
         </div>
