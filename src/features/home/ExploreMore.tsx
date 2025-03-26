3f500790d504b060f30a0ea171eef342409de67d
vsbron <vsbron@gmail.com>
2025-03-27 02:19:41 +0300

    Adjust Explore More secion for mobile

--- src/features/home/ExploreMore.tsx
+++ src/features/home/ExploreMore.tsx
@@ -13,7 +13,7 @@ function ExploreMore() {
   return (
     <div className="relative">
       <Heading as="h2">More content to explore</Heading>
-      <div className="w-3/5">
+      <div className="w-full md:w-3/5 max-md:text-[1.4rem]">
         <p>
           Dive into our expansive collection of movies and TV shows. Explore
           everything from action-packed blockbusters and heartwarming dramas to
@@ -56,11 +56,11 @@ function ExploreMore() {
         )}
       </div>
       <div
-        className="absolute -top-10 -bottom-16 right-0 w-[50%] -z-10 bg-center overflow-hidden opacity-80"
+        className="absolute -top-10 -bottom-16 right-0 w-[100%] md:w-[50%] -z-10 bg-center overflow-hidden opacity-80"
         style={{ perspective: "50rem" }}
       >
         <div
-          className="absolute -top-1/4 right-0 h-[150%] w-[80rem] bg-[100%_auto] exploreMoreBg animate-exploreMoreBg"
+          className="absolute -top-1/4 right-0 h-[150%] w-[40rem] lg:w-[60rem] xl:w-[80rem] bg-[100%_auto] exploreMoreBg animate-exploreMoreBg max-md:opacity-30"
           style={{
             transform: "rotateY(-15deg)",
             transformOrigin: "right center",
