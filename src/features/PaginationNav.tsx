c46290b6b8725c8d9a33cf9dc7230211a6b05590
vsbron <vsbron@gmail.com>
2025-04-01 08:45:45 +0300

    Minor adjustment to Pagination component

--- src/features/PaginationNav.tsx
+++ src/features/PaginationNav.tsx
@@ -12,24 +12,20 @@ function PaginationNav({
   return (
     <>
       {/* prettier-ignore */}
-      <div className={`flex flex-col sm:flex-row items-center max-md:gap-y-6 max-md:gap-3 ${BASE_GAP_CLASS}`}>
-        <div className={`flex sm:contents ${BASE_GAP_CLASS}`}>
-          <Button disabled={page === 1} onClick={()=>handlePageChange(1)} label="Go to the first page">
-            <span><PaginationSmallArrow dir="left">&laquo;</PaginationSmallArrow> First</span>
-          </Button>
-          <Button disabled={page === 1} onClick={()=>handlePageChange(page-1)} label="Go to previous page">
-            <span><PaginationSmallArrow dir="left">&lsaquo;</PaginationSmallArrow> Prev</span>
-          </Button>
-        </div>
-        <div className="mx-6 sm:mx-12">Page <b>{page}</b> out of <b>{totalPages}</b></div>
-        <div className={`flex sm:contents ${BASE_GAP_CLASS}`}>
-          <Button disabled={page === totalPages} onClick={()=>handlePageChange(page+1)} label="Go to next page">
-            <span>Next <PaginationSmallArrow dir="right">&rsaquo;</PaginationSmallArrow></span>
-          </Button>
-          <Button disabled={page === totalPages} onClick={()=>handlePageChange(totalPages)} label="Go to the last page">
-            <span>Last <PaginationSmallArrow dir="right">&raquo;</PaginationSmallArrow></span>
-          </Button>
-        </div>
+      <div className={`flex items-center max-md:gap-y-6 max-md:gap-3 ${BASE_GAP_CLASS}`}>
+        <Button disabled={page === 1} onClick={()=>handlePageChange(1)} label="Go to the first page">
+          <span><PaginationSmallArrow dir="left">&laquo;</PaginationSmallArrow><span className="max-sm:hidden"> First</span></span>
+        </Button>
+        <Button disabled={page === 1} onClick={()=>handlePageChange(page-1)} label="Go to previous page">
+          <span><PaginationSmallArrow dir="left">&lsaquo;</PaginationSmallArrow> Prev</span>
+        </Button>
+      <div className="text-[1.4rem] xs:text-[1.5rem] mx-2 xs:mx-6 sm:mx-12">Page <b>{page}</b>/<b>{totalPages}</b></div>
+        <Button disabled={page === totalPages} onClick={()=>handlePageChange(page+1)} label="Go to next page">
+          <span>Next <PaginationSmallArrow dir="right">&rsaquo;</PaginationSmallArrow></span>
+        </Button>
+        <Button disabled={page === totalPages} onClick={()=>handlePageChange(totalPages)} label="Go to the last page">
+          <span><span className="max-sm:hidden">Last </span><PaginationSmallArrow dir="right">&raquo;</PaginationSmallArrow></span>
+        </Button>
       </div>
     </>
   );
